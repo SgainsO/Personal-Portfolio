50ca220f312db4b49b2abecbb358f8d18f1e65cc
jas <jptjasc@gmail.com>
2024-10-27 13:51:47 -0400

    wrote up some descriptions

---
+++ src/media/prof_projects/prof_descriptions.js
@@ -0,0 +1,9 @@
+const descmap = [
+    {description: "Semester long project made to simulate a social media for advertising events", language: "Javascript(React Native, Express JS)"},
+    {description: "An app for helping those with an interst in Gym to track their progress", language: "Java (Android Studio)"},
+    {desctiption: "An app that utualizes ai created assets (Sound and Visual) along with Binuaral speach to improve the medical experience", language: "C#"}
+]
+
+
+
+export default descmap 
